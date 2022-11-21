78b160b883d23283572f68d645f8d7a4e010e76f
Ryan Strawser <strawser.ryan@gmail.com>
2022-11-21 13:39:29 -0500

    Update constants.ts

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,2 +1,2 @@
-export const ballotContractAddress: string = "";
+export const ballotContractAddress: string = "0xEe69666C8670D4C1C632D1d34eba1b803C218479";
 export const tokenContractAddress: string = "0x284A7042be8749c1b3A35509F27ebb09c2737956";
